176beb94d52750aca97ae9be87e8c2e7a6c2f556
Luigi Mortaro <luigi.mortaro@gmail.com>
2014-12-23 13:37:10 +0100

    Code cleaning.

--- app/src/main/java/com/gcw_rome_2014/saveme/calendar/CalendarInterface.java
+++ app/src/main/java/com/gcw_rome_2014/saveme/calendar/CalendarInterface.java
@@ -1,8 +1,12 @@
 package com.gcw_rome_2014.saveme.calendar;
 
+import java.util.Calendar;
+
 /**
- * Created by Alessio on 19/12/2014.
+ * Created by Luigi Mortaro on 19/12/2014.
+ *
+ * This Interface has the scope to decouple the technology used to
  */
 public interface CalendarInterface {
-
+    public long addEvent(String eventName, Calendar dateAndTime, int HoursPerDay);
 }
