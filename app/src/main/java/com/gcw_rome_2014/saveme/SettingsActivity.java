8937d41fae82a7f949c7760564e475e0efc86f24
Luigi Mortaro <luigi.mortaro@gmail.com>
2015-01-05 15:57:00 +0100

    Added menu_settings.xml

--- app/src/main/java/com/gcw_rome_2014/saveme/SettingsActivity.java
+++ app/src/main/java/com/gcw_rome_2014/saveme/SettingsActivity.java
@@ -24,7 +24,7 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 }
