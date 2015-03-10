ec514427a232e7d5a239e3177e9f61f48ff80290
Alicja Baszak <alicjabaszak@gmail.com>
2015-03-10 02:04:41 +0100

    Validation of number of hours in settings. Expected value between 1 and 12.

--- app/src/main/java/com/gcw_rome_2014/quickstudy/SettingsActivity.java
+++ app/src/main/java/com/gcw_rome_2014/quickstudy/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.gcw_rome_2014.quickstudy;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +14,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by Alessio on 05/01/2015.
@@ -22,8 +25,6 @@ public class SettingsActivity extends PreferenceActivity {
     public static int DIFFICULTY_HOURS_MEDIUM = 3;
     public static int DIFFICULTY_HOURS_HARD = 6;
 
-
-
     @Override
     @SuppressWarnings("deprecation")
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,10 @@ public class SettingsActivity extends PreferenceActivity {
         final EditTextPreference preferenceEasy = (EditTextPreference)getPreferenceScreen().findPreference("preference_easy");
         final EditTextPreference preferenceMedium = (EditTextPreference)getPreferenceScreen().findPreference("preference_medium");
         final EditTextPreference preferenceHard = (EditTextPreference)getPreferenceScreen().findPreference("preference_hard");
+
+        setNumberOfHoursValidation(preferenceEasy);
+        setNumberOfHoursValidation(preferenceMedium);
+        setNumberOfHoursValidation(preferenceHard);
 
         LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
         Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
@@ -55,6 +60,31 @@ public class SettingsActivity extends PreferenceActivity {
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();*/
 
+    }
+
+    private void setNumberOfHoursValidation(EditTextPreference preference) {
+        preference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                int numberOfHours = Integer.parseInt(newValue.toString());
+                if (numberOfHours >= 1 && numberOfHours <= 12) {
+                    return true;
+                } else {
+                    // Invalid value
+                    showErrorToast("The value should be between 1 and 12");
+                    return false;
+                }
+            }
+        });
+    }
+
+    private void showErrorToast(String message) {
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
 }
