7785533bd1a10e250be216a7049a6a2664e5e474
alessio-balda <ale.balda92@gmail.com>
2015-03-18 20:50:41 +0100

    Added side menu

--- app/src/main/java/com/gcw_rome_2014/quickstudy/ExamsActivity.java
+++ app/src/main/java/com/gcw_rome_2014/quickstudy/ExamsActivity.java
@@ -2,16 +2,25 @@ package com.gcw_rome_2014.quickstudy;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.gcw_rome_2014.quickstudy.ExamAdapter.OnItemClickListener;
 import com.gcw_rome_2014.quickstudy.calendar.provider.AndroidInstanceManager;
@@ -27,6 +36,13 @@ public class ExamsActivity extends ActionBarActivity {
     private ExamAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ImageButton fab;
+
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout mDrawerLinearLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private String[] mMenuTitles;
 
     private static final int RESULT_SETTINGS = 1;
 
@@ -47,7 +63,10 @@ public class ExamsActivity extends ActionBarActivity {
 
         //Show icon in the Action Bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.menu);
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         //Set default values for settings
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
@@ -82,6 +101,45 @@ public class ExamsActivity extends ActionBarActivity {
             }
         });
 
+        mMenuTitles = getResources().getStringArray(R.array.menu_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLinearLayout = (LinearLayout) findViewById(R.id.left_drawer);
+        mDrawerList = (ListView) findViewById(R.id.list_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMenuTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  // host Activity
+                mDrawerLayout,         // DrawerLayout object
+                R.drawable.ic_menu,  // nav drawer image to replace 'Up' caret
+                R.string.drawer_open,  //"open drawer" description for accessibility
+                R.string.drawer_close  //"close drawer" description for accessibility
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle("Your exams");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("QuickStudy");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
         AndroidInstanceManager instanceManager = new AndroidInstanceManager(getContentResolver());
         instanceManager.queryInstance();
     }
@@ -95,6 +153,11 @@ public class ExamsActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()) {
             case R.id.action_calendar:
                 //Open calendarProvider calendar with an intent.
@@ -102,14 +165,65 @@ public class ExamsActivity extends ActionBarActivity {
                 intent.setData(Uri.parse("content://com.android.calendar/time"));
                 startActivity(intent);
                 break;
-            case R.id.action_feedback:
-                startActivity(new Intent(this, FeedActivity.class));
-                break;
-            case R.id.action_settings:
-                startActivityForResult(new Intent(this, SettingsActivity.class), RESULT_SETTINGS);
-                break;
         }
         return true;
     }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerLinearLayout);
+        menu.findItem(R.id.action_calendar).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    /* The click listener for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                startActivityForResult(new Intent(this, SettingsActivity.class), RESULT_SETTINGS);
+                break;
+            case 4:
+                break;
+            case 5:
+                startActivity(new Intent(this, FeedActivity.class));
+                break;
+        }
+
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 
 }
